53b06ab8cc707a92fe6427a8793c3b34768838cf
EGOIST <0x142857@gmail.com>
2021-03-03 17:02:02 +0800

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+# @ream/vercel-builder
+
+```json
+{
+  "builds": [
+    {
+      "src": "ream.config.js",
+      "use": "@ream/vercel-builder"
+    }
+  ]
+}
+```
+
+## License
+
+MIT &copy; [EGOIST](https://github.com/sponsors/egoist)
---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import path from 'path'
+import fs from 'fs'
+import execa from 'execa'
+import { BuildOptions, createLambda, FileBlob, glob } from '@vercel/build-utils'
+
+export async function build(options: BuildOptions) {
+  const dir = path.dirname(options.entrypoint)
+  // Build the code here…
+  await execa('./node_modules/.bin/ream', ['build', '--standalone'], {
+    stdio: 'inherit',
+    cwd: dir,
+  })
+
+  const lambda = createLambda({
+    runtime: 'nodejs14.x',
+    handler: 'index.handler',
+    files: {
+      'index.js': new FileBlob({
+        data: await fs.promises.readFile(
+          '.ream/meta/server-context-bundle.js',
+          'utf8',
+        ),
+      }),
+    },
+  })
+
+  const staticFiles = await glob('**', path.join(dir, '.ream/client'))
+
+  return {
+    output: {
+      ...lambda,
+      ...staticFiles,
+    },
+    watch: [
+      // Dependent files to trigger a rebuild in `vercel dev` go here…
+    ],
+    routes: [{ handle: 'filesystem' }, { src: '/(.*)', dest: '/index' }],
+  }
+}
